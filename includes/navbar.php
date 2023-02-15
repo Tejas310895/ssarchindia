de3d56d1c489957f3273d8bfefab5d0d44271750
Tejas310895 <43029698+Tejas310895@users.noreply.github.com>
2023-02-15 16:51:02 +0530

    third commit

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg fixed-top bg-white">
     <div class="container-fluid">
-        <a class="navbar-brand ps-4" href="#">
+        <a class="navbar-brand ps-4" href=".">
             <img src="assets/images/logo.jpg" alt="Bootstrap" width="100">
         </a>
         <button class="navbar-toggler border border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
