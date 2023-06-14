5004061ad6a2de038b0a17407c78eae2d930c093
Tejas310895 <43029698+Tejas310895@users.noreply.github.com>
2023-06-14 23:25:17 +0530

    new renders

--- projects.php
+++ projects.php
@@ -20,215 +20,236 @@
                 <div class="filters">
                     <ul>
                         <li class="active" data-filter="*">All</li>
-                        <li data-filter=".AUDI_GMC">AUDI GMC</li>
-                        <li data-filter=".JLR_WORLI">JLR WORLI</li>
-                        <li data-filter=".MG_HECTOR">MG HECTOR</li>
-                        <li data-filter=".MODI_HOUSE">MODI HOUSE THANE</li>
+                        <li data-filter=".MDLZ">MONDELEZ</li>
+                        <li data-filter=".MDLZ_JOY">MONDELEZ JOY HOUSE</li>
+                        <li data-filter=".MG_CHE">MG DIGITAL CHENNAI</li>
+                        <li data-filter=".MG_PRA">MG PRABHADEVI</li>
                         <li data-filter=".RHP_PART">RHP PARTNERS</li>
                     </ul>
                 </div>
 
                 <div class="filters-content">
                     <div class="row grid">
-                        <div class="col-sm-4 all AUDI_GMC">
+                        <div class="col-sm-4 all MDLZ">
                             <div class="item">
-                                <img src="assets/images/projects/audi1.jpg" alt="Lorem Name">
+                                <img src="assets/images/projects/MDLZ1.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
-                        <div class="col-sm-4 all AUDI_GMC">
+                        <div class="col-sm-4 all MDLZ">
                             <div class="item">
-                                <img src="assets/images/projects/audi2.jpg" alt="Lorem Name">
+                                <img src="assets/images/projects/MDLZ2.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
-                        <div class="col-sm-4 all AUDI_GMC">
+                        <div class="col-sm-4 all MDLZ">
                             <div class="item">
-                                <img src="assets/images/projects/audi3.jpg" alt="Lorem Name">
+                                <img src="assets/images/projects/MDLZ3.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
-                        <div class="col-sm-4 all AUDI_GMC">
+                        <div class="col-sm-4 all MDLZ">
                             <div class="item">
-                                <img src="assets/images/projects/audi4.jpg" alt="Lorem Name">
+                                <img src="assets/images/projects/MDLZ4.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
-                        <div class="col-sm-4 all AUDI_GMC">
+                        <div class="col-sm-4 all MDLZ">
                             <div class="item">
-                                <img src="assets/images/projects/audi5.png" alt="Lorem Name">
+                                <img src="assets/images/projects/MDLZ5.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
-                        <div class="col-sm-4 all AUDI_GMC">
+                        <div class="col-sm-4 all MDLZ">
                             <div class="item">
-                                <img src="assets/images/projects/audi6.jpg" alt="Lorem Name">
+                                <img src="assets/images/projects/MDLZ6.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
-                        <div class="col-sm-4 all AUDI_GMC">
+                        <div class="col-sm-4 all MDLZ_JOY">
                             <div class="item">
-                                <img src="assets/images/projects/audi7.jpg" alt="Lorem Name">
+                                <img src="assets/images/projects/MDLZJ1.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
-                        <div class="col-sm-4 all AUDI_GMC">
+                        <div class="col-sm-4 all MDLZ_JOY">
                             <div class="item">
-                                <img src="assets/images/projects/audi8.jpg" alt="Lorem Name">
+                                <img src="assets/images/projects/MDLZJ2.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
-                        <div class="col-sm-4 all JLR_WORLI">
+                        <div class="col-sm-4 all MDLZ_JOY">
                             <div class="item">
-                                <img src="assets/images/projects/jlr1.png" alt="Lorem Name">
+                                <img src="assets/images/projects/MDLZJ3.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
-                        <div class="col-sm-4 all JLR_WORLI">
+                        <div class="col-sm-4 all MDLZ_JOY">
                             <div class="item">
-                                <img src="assets/images/projects/jlr2.png" alt="Lorem Name">
+                                <img src="assets/images/projects/MDLZJ4.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
-                        <div class="col-sm-4 all JLR_WORLI">
+                        <div class="col-sm-4 all MDLZ_JOY">
                             <div class="item">
-                                <img src="assets/images/projects/jlr3.png" alt="Lorem Name">
+                                <img src="assets/images/projects/MDLZJ5.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
-                        <div class="col-sm-4 all JLR_WORLI">
+                        <div class="col-sm-4 all MDLZ_JOY">
                             <div class="item">
-                                <img src="assets/images/projects/jlr4.png" alt="Lorem Name">
+                                <img src="assets/images/projects/MDLZJ6.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
-                        <div class="col-sm-4 all JLR_WORLI">
+                        <div class="col-sm-4 all MG_CHE">
                             <div class="item">
-                                <img src="assets/images/projects/jlr5.png" alt="Lorem Name">
+                                <img src="assets/images/projects/MGC1.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
-                        <div class="col-sm-4 all JLR_WORLI">
+                        <div class="col-sm-4 all MG_CHE">
                             <div class="item">
-                                <img src="assets/images/projects/jlr6.png" alt="Lorem Name">
+                                <img src="assets/images/projects/MGC2.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
-                        <div class="col-sm-4 all MG_HECTOR">
+                        <div class="col-sm-4 all MG_CHE">
                             <div class="item">
-                                <img src="assets/images/projects/mg1.jpg" alt="Lorem Name">
+                                <img src="assets/images/projects/MGC3.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
-                        <div class="col-sm-4 all MG_HECTOR">
+                        <div class="col-sm-4 all MG_CHE">
                             <div class="item">
-                                <img src="assets/images/projects/mg2.jpg" alt="Lorem Name">
+                                <img src="assets/images/projects/MGC4.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
-                        <div class="col-sm-4 all MG_HECTOR">
+                        <div class="col-sm-4 all MG_CHE">
                             <div class="item">
-                                <img src="assets/images/projects/mg3.png" alt="Lorem Name">
+                                <img src="assets/images/projects/MGC5.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
-                        <div class="col-sm-4 all MG_HECTOR">
+                        <div class="col-sm-4 all MG_CHE">
                             <div class="item">
-                                <img src="assets/images/projects/mg4.png" alt="Lorem Name">
+                                <img src="assets/images/projects/MGC6.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
-                        <div class="col-sm-4 all MODI_HOUSE">
+                        <div class="col-sm-4 all MG_PRA">
                             <div class="item">
-                                <img src="assets/images/projects/mh1.png" alt="Lorem Name">
+                                <img src="assets/images/projects/MGP1.jpg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
-                        <div class="col-sm-4 all MODI_HOUSE">
+                        <div class="col-sm-4 all MG_PRA">
                             <div class="item">
-                                <img src="assets/images/projects/mh2.png" alt="Lorem Name">
+                                <img src="assets/images/projects/MGP2.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
-                        <div class="col-sm-4 all MODI_HOUSE">
+                        <div class="col-sm-4 all MG_PRA">
                             <div class="item">
-                                <img src="assets/images/projects/mh3.png" alt="Lorem Name">
+                                <img src="assets/images/projects/MGP3.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
-                        <div class="col-sm-4 all MODI_HOUSE">
+                        <div class="col-sm-4 all MG_PRA">
                             <div class="item">
-                                <img src="assets/images/projects/mh4.png" alt="Lorem Name">
+                                <img src="assets/images/projects/MGP4.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
-                        <div class="col-sm-4 all RHP_PART">
+                        <div class="col-sm-4 all MG_PRA">
                             <div class="item">
-                                <img src="assets/images/projects/rhp1.png" alt="Lorem Name">
+                                <img src="assets/images/projects/MGP5.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
-                        <div class="col-sm-4 all RHP_PART">
+                        <div class="col-sm-4 all MG_PRA">
                             <div class="item">
-                                <img src="assets/images/projects/rhp2.png" alt="Lorem Name">
-                                <!-- <div class="p-inner">
-                                </div> -->
-                            </div>
-                        </div>
-                        <div class="col-sm-4 all RHP_PART">
-                            <div class="item">
-                                <img src="assets/images/projects/rhp3.png" alt="Lorem Name">
+                                <img src="assets/images/projects/MGP6.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
                         <div class="col-sm-4 all RHP_PART">
                             <div class="item">
-                                <img src="assets/images/projects/rhp4.png" alt="Lorem Name">
+                                <img src="assets/images/projects/RHP1.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
                         <div class="col-sm-4 all RHP_PART">
                             <div class="item">
-                                <img src="assets/images/projects/rhp5.png" alt="Lorem Name">
+                                <img src="assets/images/projects/RHP2.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
                         <div class="col-sm-4 all RHP_PART">
                             <div class="item">
-                                <img src="assets/images/projects/rhp6.png" alt="Lorem Name">
+                                <img src="assets/images/projects/RHP3.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
                         </div>
                         <div class="col-sm-4 all RHP_PART">
                             <div class="item">
-                                <img src="assets/images/projects/rhp7.png" alt="Lorem Name">
+                                <img src="assets/images/projects/RHP4.jpeg" alt="Lorem Name">
+                                <!-- <div class="p-inner">
+                                </div> -->
+                            </div>
+                        </div>
+                        <div class="col-sm-4 all RHP_PART">
+                            <div class="item">
+                                <img src="assets/images/projects/RHP5.jpeg" alt="Lorem Name">
+                                <!-- <div class="p-inner">
+                                </div> -->
+                            </div>
+                        </div>
+                        <div class="col-sm-4 all RHP_PART">
+                            <div class="item">
+                                <img src="assets/images/projects/RHP6.jpeg" alt="Lorem Name">
+                                <!-- <div class="p-inner">
+                                </div> -->
+                            </div>
+                        </div>
+                        <div class="col-sm-4 all RHP_PART">
+                            <div class="item">
+                                <img src="assets/images/projects/RHP7.jpeg" alt="Lorem Name">
+                                <!-- <div class="p-inner">
+                                </div> -->
+                            </div>
+                        </div>
+                        <div class="col-sm-4 all RHP_PART">
+                            <div class="item">
+                                <img src="assets/images/projects/RHP8.jpeg" alt="Lorem Name">
                                 <!-- <div class="p-inner">
                                 </div> -->
                             </div>
